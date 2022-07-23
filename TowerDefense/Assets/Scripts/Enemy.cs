18cbef25ffe070ae974c60cb254ef898e94616c0
juanKovac <csn99csn@Outlook.com>
2022-07-23 16:25:06 +0800

    Health Bar of Enemy

--- TowerDefense/Assets/Scripts/Enemy.cs
+++ TowerDefense/Assets/Scripts/Enemy.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+ using UnityEngine.UI;
 
 
 public class Enemy : MonoBehaviour
@@ -10,16 +11,24 @@ public class Enemy : MonoBehaviour
     [HideInInspector]
     public float speed;
 
-    public float health = 100;
+    public float startHealth = 100;
+    public float health;
     public int enemyWorth = 100;
+
+    [Header("Unity Stuff")]
+    public Image healthBar;
 
     void Start()
     {
         speed = startSpeed;
+        health = startHealth;
     }
     public void TakeDamege(float amount)
     {
         health -= amount;
+
+        healthBar.fillAmount = health / startHealth;
+
         if (health <= 0)
         {
             Die();
